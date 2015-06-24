02b77548f95c0397446fef7258bc1d53b8444635
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-24 11:20:54 +0600

    First commit

---
+++ Day 4 Photo Bombers App/PhotoCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Almas T on 6/24/15.
+//  Copyright (c) 2015 Almas Tuyakbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) UIImageView *imageView;
+
+@end
--- Day 4 Photo Bombers App/ViewController.m
+++ Day 4 Photo Bombers App/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "PhotoCollectionViewCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource,UICollectionViewDelegate>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -16,12 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self.collectionView registerClass:[PhotoCollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionView methods
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.imageView.image = [UIImage imageNamed:@"bomb.jpg"];
+    
+    return cell;
 }
 
 @end
