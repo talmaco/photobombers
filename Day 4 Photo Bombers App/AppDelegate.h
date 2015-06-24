fe963c2283f72be5b0b945fc532dd3437c34c758
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-24 10:31:56 +0600

    Initial Commit

---
+++ Day 4 Photo Bombers App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Almas T on 6/24/15.
+//  Copyright (c) 2015 Almas Tuyakbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day 4 Photo Bombers App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day 4 Photo Bombers App
+//
+//  Created by Almas T on 6/24/15.
+//  Copyright (c) 2015 Almas Tuyakbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
