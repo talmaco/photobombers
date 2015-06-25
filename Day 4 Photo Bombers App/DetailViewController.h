e495a04e4214ca7e3e9e65dc269439e2fc23f680
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-25 14:46:00 +0600

    Added Detailed view for images

---
+++ Day 4 Photo Bombers App/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Almas T on 6/25/15.
+//  Copyright (c) 2015 Almas Tuyakbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *photo;
+
+@end
--- Day 4 Photo Bombers App/ViewController.m
+++ Day 4 Photo Bombers App/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "PhotoCollectionViewCell.h"
 #import <SimpleAuth/SimpleAuth.h>
+#import "DetailViewController.h"
 
 @interface ViewController () <UICollectionViewDataSource,UICollectionViewDelegate>
 
@@ -102,6 +103,18 @@
     cell.photo = photo;
     
     return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *photo = self.photos[indexPath.row];
+    
+    DetailViewController *viewController = [DetailViewController new];
+
+    viewController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    viewController.photo = photo;
+    
+    [self presentViewController:viewController animated:YES completion:nil];
 }
 
 @end
